c3d666abf2cc89de421947428b39a6a1db54318a
Digvijay Kadam <digvi@192.168.0.100>
2023-09-27 18:25:19 +0530

    server code uploded succesfully

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors')
+const app = express();
+require('dotenv').config();
+const port = process.env.PORT || 4000;
+
+const apiKey = process.env.API_KEY;
+
+app.use(express.json());
+app.use(cors());
+app.get('/api/weather/:city', async (req, res) => {
+  try {
+    const { city } = req.params;
+    const weatherData = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`);
+    res.json(weatherData.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
